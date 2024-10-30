8cf739e172da4290aaf12a61cd2c2535899f5a93
taruncse2022 <118814372+taruncse2022@users.noreply.github.com>
2024-10-31 01:19:41 +0530

    isValidBST

---
+++ Binary_Search_Tree/isValidBST.java
@@ -0,0 +1,57 @@
+public class BST{
+    static class Node{
+        Node left;
+        Node right;
+        int data;
+        
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data > val){
+            //left subtree
+            root.left = insert(root.left , val);
+        }else{
+            //right subtree
+           root.right = insert(root.right , val); 
+        }
+        return root;
+    }
+    public static boolean isValidBST(Node root , Node min, Node max){
+        if(root == null){
+            return true;
+        }
+        if(min !=null && root.data <=min.data){
+            return false;
+        }
+        else if(max != null && root.data >= max.data){
+            return false;
+        }
+        return isValidBST(root.left , min, root)
+            && isValidBST(root.right, root, max);
+    }
+    
+    public static void main(String args[]){
+        int values[]={8,5,3,6,10,11,14};
+        Node root = null;
+        
+        for(int i =0; i<values.length; i++){
+            root = insert(root, values[i]);
+        }
+        // inOrder(root);
+        // System.out.println();
+        
+        if(isValidBST(root,null,null)){
+            System.out.println("valid");
+        }else{
+            System.out.println("Invalid");
+        }
+    }
+}
